d52de6bb3c4e60f2a52ee47a517a70c80bc8e1b5
huss <hussein_beydoun_@outlook.com>
2025-05-23 19:07:39 -0400

    Draw triangle

--- venv/main.py
+++ venv/main.py
@@ -1,19 +1,30 @@
 import pygame
 from constants import *
+from player import Player
 
 
 
 def main():
 	print(f"Starting Asteroids! \nScreen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
 
+
 	pygame.init()
+	clock = pygame.time.Clock()
+	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	x =  SCREEN_WIDTH / 2
+	y = SCREEN_HEIGHT / 2
+	player = Player(x, y)
+	
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-		screen.fill((0, 0, 0))
+		screen.fill(BLACK)
+		player.draw(screen)
 		pygame.display.flip()
+		clock.tick(60)
+		dt = clock.tick(60) / 1000.0
 
 if __name__ == "__main__":
 	main()
